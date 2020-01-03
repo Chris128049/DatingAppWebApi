4926933e62b6059cab3aab9b9777ef0aae4beede
Chris Nava <chris128049@gmail.com>
2020-01-03 11:15:09 -0800

    updated controllers and added security authentication

--- DatingApp.API/Controllers/AuthController.cs
+++ DatingApp.API/Controllers/AuthController.cs
@@ -19,9 +19,9 @@ namespace DatingApp.API.Controllers
         [HttpPost("register")]
         public async Task<IActionResult> Register(UserForRegisterDto userForRegisterDto)
         {
-            //validate request
-
+           
             userForRegisterDto.Username = userForRegisterDto.Username.ToLower();
+
             if (await _repo.UserExists(userForRegisterDto.Username))
                 return BadRequest("Username already exists");
             
